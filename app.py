343f72a4235533673c69b9ca257ac417304fbcb3
Kjetil Vaagen <kjetivaa@stud.ntnu.no>
2017-05-09 11:46:52 +0200

    bug

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def verify():
     #Webhook verification
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
