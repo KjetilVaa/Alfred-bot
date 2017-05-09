0a99fe3e28260f5e256cc0a81a1e281a2c8bdb33
Kjetil Vaagen <kjetivaa@stud.ntnu.no>
2017-05-09 11:59:45 +0200

    echo function

--- app.py
+++ app.py
@@ -1,20 +1,59 @@
 import os, sys
 
 from flask import Flask, request
+from pymessenger import Bot
 
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+PAGE_ACCESS_TOKEN = "EAADyBpRsToMBAA38IirkkrfZCyk7CD9C7eN6GWdTK0yCg7n19k2XewzM9uAuTNvDlzZBl5CaCQfZCByUYctDzNVz81bDQmCNZA9cfNAHh88mkeaeMUQZCljwnuYdZAEsPxNBx37xImpdUYZA5FTUYEkNzwbZBg67JtzZC6AypPiMY7QZDZD"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
+
+
+@app.route('/', methods=['GET'])
 def verify():
-    #Webhook verification
+	# Webhook verification
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == "alfred_success":
+        if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-    return "Hello World", 200
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+	data = request.get_json()
+	log(data)
+
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
+
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'no text'
+
+					# Echo
+					response = messaging_text
+					bot.send_text_message(sender_id, response)
+
+	return "ok", 200
+
+
+def log(message):
+	print(message)
+	sys.stdout.flush()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=80)
+	app.run(debug = True, port = 80)
+
 
