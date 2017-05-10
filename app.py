083639719d63b7d8849ecb6d218cfec0d099e9c3
Kjetil Vaagen <kjetivaa@stud.ntnu.no>
2017-05-10 13:13:25 +0200

    ha

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def sendPictureJson(url, senderid):
 
 	r = requests.post(
 	"https://graph.facebook.com/v2.6/me/messages?access_token=EAADyBpRsToMBAA38IirkkrfZCyk7CD9C7eN6GWdTK0yCg7n19k2XewzM9uAuTNvDlzZBl5CaCQfZCByUYctDzNVz81bDQmCNZA9cfNAHh88mkeaeMUQZCljwnuYdZAEsPxNBx37xImpdUYZA5FTUYEkNzwbZBg67JtzZC6AypPiMY7QZDZD"
-		,json_data, params)
+		,json_data)
 	print(r, r.status_code, r.text)
 
 
